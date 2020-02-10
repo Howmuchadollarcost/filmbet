8a11f12e9aa66c5f157506eb5af84a98c4a804ed
Howmuchadollarcost <muluyonas@yahoo.com>
2020-02-10 15:44:11 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from 'react-router-dom';
 
-function App() {
+import "./App.css";
+
+import Navbar from './component/Navbar/Navbar';
+import FrontPage from './container/FrontPage/FrontPage'; 
+import ActiveComponent from './component/ActiveComponent/ActiveComponent';
+
+const App =  () =>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path ="/" component = {FrontPage} />
+        <Route exact path ="/:id" component = {ActiveComponent} />
+      </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
